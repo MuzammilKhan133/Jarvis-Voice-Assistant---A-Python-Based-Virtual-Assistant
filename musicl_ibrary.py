21b23dd773d5e50e6b83f31178acf00aeb5eecb3
Muhammad Muzammil Khan <muzammil.khattak50@gmail.com>
2024-08-06 20:53:41 +0500

    musicl_ibrary.py

---
+++ musicl_ibrary.py
@@ -0,0 +1,7 @@
+#More songs can be added according to the user's preferences
+music = {
+    "courtesy call":"https://youtu.be/yLN9tGbGlz8?si=67_MCL7douslMLQ9",
+    "frank" : "https://youtu.be/pdnQ4D6Vo1k?si=obbuXqCq5ALSAmgD",
+    "skyfall": "https://youtu.be/sZrTJesvJeo?si=0VYE1qBksZW3B1Fu",
+    "fainted":"https://youtu.be/dJWFUBAUM0E?si=T86sEgWFySpmZg-C"
+}
